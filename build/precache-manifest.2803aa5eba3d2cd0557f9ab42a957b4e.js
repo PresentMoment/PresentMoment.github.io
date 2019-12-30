e9b57991f84b47c24f591646efb262b76bae7069
Robert Girardin <robertlgirardin@gmail.com>
2019-12-30 12:01:13 +0100

    deploy build

---
+++ build/precache-manifest.2803aa5eba3d2cd0557f9ab42a957b4e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c412a039c76541f5cfde10e62ca3320e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea2afe3944d2ac859116",
+    "url": "/static/css/main.42cdf6b0.chunk.css"
+  },
+  {
+    "revision": "0a350c1967284351962a",
+    "url": "/static/js/2.821ff0a3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.821ff0a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea2afe3944d2ac859116",
+    "url": "/static/js/main.aa95be92.chunk.js"
+  },
+  {
+    "revision": "3287873f55ee158290b1",
+    "url": "/static/js/runtime-main.236cb116.js"
+  },
+  {
+    "revision": "4fb7f831db1fd6e6862ac98d573b2d64",
+    "url": "/static/media/artist.4fb7f831.png"
+  },
+  {
+    "revision": "0d2a5a02d77de870b382a503de724701",
+    "url": "/static/media/deed.0d2a5a02.png"
+  },
+  {
+    "revision": "745da71af050acda847c791ed67624f8",
+    "url": "/static/media/finn.745da71a.png"
+  },
+  {
+    "revision": "30c0474fd6bc60335ec063e3e5f46424",
+    "url": "/static/media/just.30c0474f.png"
+  },
+  {
+    "revision": "220967529c4edb82cd25a01de5335ae1",
+    "url": "/static/media/material.22096752.png"
+  },
+  {
+    "revision": "901efd49f46f66739dc08e1146b9cf93",
+    "url": "/static/media/simma.901efd49.png"
+  }
+]);

ac5e795e209622fdbd3f75fbfa691e967a049324
Robert Girardin <robertlgirardin@gmail.com>
2020-01-13 20:58:18 +0100

    force commit portfolio data

---
+++ src/Components/portfolioItems.js
@@ -0,0 +1,68 @@
+import artist from "../images/artist.png";
+import just from "../images/just.png";
+import material from "../images/material.png";
+import simma from "../images/simma.png";
+import deed from "../images/deed.png";
+import finn from "../images/finn.png";
+import anthromes from "../images/anthromes.png";
+
+const portfolioItems = {
+  deed: {
+    name: "The Deed",
+    description:
+      "Working as a Junior Developer to help launch multi-platform, international website. From design mockups in Storybooks to engineering components in React and React Native, to tying everything to the Node.JS backend.  Utilizing TypeScript superset throughout.",
+    link: "https://www.thedeed.app/",
+    gitLink: "",
+    image: deed
+  },
+
+  material: {
+    name: "Material",
+    description:
+      "A RESTful, location based app for users to find public sculpture near them and post public sculptures they find in the world. Built with Handlebars, Express and MongoDB.",
+    link: "https://materialism.herokuapp.com/",
+    gitLink: "https://github.com/PresentMoment/Material",
+    image: material
+  },
+  robertgirardin: {
+    name: "robertgirardin.net",
+    description:
+      "Fully responsive, SPA portfolio site built with React and CSS.",
+    link: "http://robertgirardin.net/",
+    gitLink: "https://github.com/PresentMoment/creative-portfolio",
+    image: artist
+  },
+  simma: {
+    name: "Simma",
+    description:
+      "A spatial, sound exploration tool built with React and Tone.JS",
+    link: "http://simma.herokuapp.com/",
+    gitLink: "https://github.com/PresentMoment/simma",
+    image: simma
+  },
+  financialAuralizer: {
+    name: "Financial Auralizer",
+    description: "Using Tone.JS to 'auralize' financial data",
+    link: "https://presentmoment.github.io/Financial-Auralizer/",
+    gitLink: "https://github.com/PresentMoment/Financial-Auralizer",
+    image: finn
+  },
+  justIntonation: {
+    name: "Just Intonation",
+    description:
+      "An educational tool to learn about just intonation. Built with P5 and Tone.JS",
+    link: "https://presentmoment.github.io/Just/",
+    gitLink: "https://github.com/PresentMoment/Just",
+    image: just
+  },
+  Anthromes: {
+    name: "Anthromes",
+    description:
+      "Admin and contributor for experimental, JavaScript based imaging platform.",
+    link: "",
+    gitLink: "https://github.com/Anthromes",
+    image: anthromes
+  }
+};
+
+export default portfolioItems;

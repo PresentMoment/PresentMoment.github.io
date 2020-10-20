c4d26ed1d82576a3d8a4144cca04daf9ea7cfc7a
Robert Girardin <robertlgirardin@gmail.com>
2020-10-20 09:55:09 -0400

    replace missing files

---
+++ src/Components/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="about">
+      <p>
+        I'm Robert, a front end javascript developer focusing on the React
+        ecosystem. Before beginning my career as a developer, I worked as a
+        location manager and scout in the film industry. In that job
+        communication was the lynchpin of a success. I believe communication is
+        still paramount in my current field, both within dev teams and in the
+        writing of clean, readable code. Click around to see what I've been up
+        to, and how to contact me.
+      </p>
+    </div>
+  );
+}
